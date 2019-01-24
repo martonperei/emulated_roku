651328a1fb2eccf81be41a9f60043f7b29354f84
Marton Perei <mindig.marton@gmail.com>
2019-01-24 20:54:18 +0100

    Release 0.1.8

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name="emulated_roku",
-      version="0.1.7",
+      version="0.1.8",
       description="Library to emulate a roku server to serve as a proxy"
                   "for remotes such as Harmony",
       url="https://gitlab.com/mindig.marton/emulated_roku",
       download_url="https://gitlab.com"
                    "/mindig.marton/emulated_roku"
-                   "/repository/archive.zip?ref=0.1.7",
+                   "/repository/archive.zip?ref=0.1.8",
       author="mindigmarton",
       license="MIT",
       packages=["emulated_roku"],
